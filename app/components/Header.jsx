47b1b96d61529f6c0b2d1c386304ea3cebc94a0a
Yasheeb_Faridi <126257671+yasheeb-faridi@users.noreply.github.com>
2025-11-19 13:10:22 +0530

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         </p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
           <a href='#contact' className='px-8 py-2 border border-gray-500 rounded-full bg-black text-white flex items-center gap-4'>contact me <Image src={assets.right_arrow_white} alt='arr-wh' className="rounded-full w-4"/> </a>
-          <a href='/Yasheeb_faridi-hackerresume.pdf' className='px-8 py-2 border border-gray-500 rounded-full flex items-center gap-2'>Download Resume<Image src={assets.download_icon} alt='dwn-icon' className="rounded-full w-5"/> </a>
+          <a href='/Yasheeb_Faridi-hackerresume.pdf' className='px-8 py-2 border border-gray-500 rounded-full flex items-center gap-2'>Download Resume<Image src={assets.download_icon} alt='dwn-icon' className="rounded-full w-5"/> </a>
           <a href='https://github.com/yasheeb-faridi' className='px-8 py-2 border border-gray-500 rounded-full flex items-center gap-2'>Visit My GitHub<Image src={assets.github} alt='github' className="rounded-full w-6"/> </a>
         </div>
 
